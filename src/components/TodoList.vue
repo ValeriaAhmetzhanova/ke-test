77e964fd5098b456c7a59ddd286b04668fc14376
Valeria <v.ahmetzhanova@gmail.com>
2020-08-21 01:23:44 +0500

    drag and drop

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,36 +3,72 @@
         <b-row>
             <b-col cols="4">
                 <h4>To Do</h4>
-                <ul>
-                    <TodoItem
-                            v-for="todo in getProjectById(id).tasks"
-                            v-if="todo.status === 'to do'"
-                            v-bind:todo="todo"
-                            v-bind:projectId="id"
-                    />
-                </ul>
+                <div
+                        class="drop-zone"
+                        @drop='onDrop($event, "to do")'
+                        @dragover.prevent
+                        @dragenter.prevent
+                >
+                    <ul>
+                        <li
+                                v-for="todo in getProjectById(id).tasks"
+                                v-if="todo.status === 'to do'"
+                                draggable
+                                @dragstart="startDrag($event, todo)">
+                            <TodoItem
+                                    v-bind:todo="todo"
+                                    v-bind:projectId="id"
+                                    class="drag-el"
+                            />
+                        </li>
+                    </ul>
+                </div>
             </b-col>
             <b-col cols="4">
                 <h4>In Progress</h4>
-                <ul>
-                    <TodoItem
-                            v-for="todo in getProjectById(id).tasks"
-                            v-if="todo.status === 'in progress'"
-                            v-bind:todo="todo"
-                            v-bind:projectId="id"
-                    />
-                </ul>
+                <div
+                        class="drop-zone"
+                        @drop='onDrop($event, "in progress")'
+                        @dragover.prevent
+                        @dragenter.prevent
+                >
+                    <ul>
+                        <li
+                                v-for="todo in getProjectById(id).tasks"
+                                v-if="todo.status === 'in progress'"
+                                draggable
+                                @dragstart="startDrag($event, todo)">
+                            <TodoItem
+                                    v-bind:todo="todo"
+                                    v-bind:projectId="id"
+                                    class="drag-el"
+                            />
+                        </li>
+                    </ul>
+                </div>
             </b-col>
             <b-col cols="4">
                 <h4>Done</h4>
-                <ul>
-                    <TodoItem
-                            v-for="todo in getProjectById(id).tasks"
-                            v-if="todo.status === 'done'"
-                            v-bind:todo="todo"
-                            v-bind:projectId="id"
-                    />
-                </ul>
+                <div
+                        class="drop-zone"
+                        @drop='onDrop($event, "done")'
+                        @dragover.prevent
+                        @dragenter.prevent
+                >
+                    <ul>
+                        <li
+                                v-for="todo in getProjectById(id).tasks"
+                                v-if="todo.status === 'done'"
+                                draggable
+                                @dragstart="startDrag($event, todo)">
+                            <TodoItem
+                                    v-bind:todo="todo"
+                                    v-bind:projectId="id"
+                                    class="drag-el"
+                            />
+                        </li>
+                    </ul>
+                </div>
             </b-col>
         </b-row>
     </div>
@@ -42,14 +78,41 @@
     import {mapGetters, mapMutations} from "vuex";
     import TodoItem from '@/components/TodoItem'
     export default {
-        computed: mapGetters(["getProjectById"]),
+        computed: mapGetters(["allProjects","getProjectById"]),
         props: ['id'],
         components: {
             TodoItem
+        },
+        methods: {
+            ...mapMutations(["updateProjects"]),
+            startDrag (evt, item) {
+                evt.dataTransfer.dropEffect = 'move';
+                evt.dataTransfer.effectAllowed = 'move';
+                evt.dataTransfer.setData('itemID', item.id);
+
+            },
+            onDrop (evt, list) {
+                let itemID = evt.dataTransfer.getData('itemID');
+                let updatedProjects = this.allProjects;
+                updatedProjects.find(project => project.id == this.id).tasks.find(task => task.id == itemID).status = list;
+                this.updateProjects(
+                    updatedProjects
+                );
+            }
         }
     }
 </script>
 
 <style>
+    .drop-zone {
+        background-color: #eee;
+        margin-bottom: 10px;
+        padding: 10px;
+    }
 
+    .drag-el {
+        background-color: #fff;
+        margin-bottom: 10px;
+        padding: 5px;
+    }
 </style>
